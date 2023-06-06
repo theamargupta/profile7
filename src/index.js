4e8cc976afe8d450fde8c4b5ba6dec3d1cac90ab
Amar Gupta <theamargupta.tech@gmail.com>
2023-06-06 15:45:48 +0530

    changes pushed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 //  Component
-import App from './App';
+import App from './app';
 
 ReactDOM.render(
   <BrowserRouter>
